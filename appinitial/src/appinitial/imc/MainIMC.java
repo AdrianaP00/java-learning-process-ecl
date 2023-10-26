311bd251a61687dc49896c70a1cac1e1e852efbf
Adriana Piccolo <adrianapiccolo00@gmail.com>
2023-10-26 13:12:53 +0200

    changes in my repo

---
+++ appinitial/src/appinitial/imc/MainIMC.java
@@ -0,0 +1,46 @@
+package appinitial.imc;
+
+import java.util.Scanner;
+
+public class MainIMC {
+	public static void main(String[] args) {
+		/*
+		 * PROGRAMA IMC Índice de Masa Corporal
+		 * 
+		 * Fórmula IMC = peso kg / altura al cuadrado
+		 * 
+		 * IMC < 16 DESNUTRIDO IMC >=16 Y < 18 DELGADO IMC >=18 Y < 25 IDEAL IMC >=25 Y
+		 * < 31 SOBREPESO IMC >= 31 OBESO
+		 * 
+		 * TODO: HACED UNA APP QUE PIDA AL USUARIO SU PESO Y SU ESTATURA Y LE INFORME DE
+		 * SU IMC NUMÉRICO Y NOMIMAL
+		 * 
+		 * PEJ: 22.35 IDEAL
+		 */
+		Scanner scanner = new Scanner (System.in);
+		System.out.println("peso kg");
+		float peso = scanner.nextFloat();
+		System.out.println("altura cm");
+		float altura = scanner.nextFloat();
+		
+		String imcNominal = null;
+		double imcNumerico= peso / Math.pow(altura, 2);
+		
+		if (imcNumerico<16)
+		{
+			imcNominal ="DESNUTRIDO";
+		} else if (imcNumerico>=16 &&  imcNumerico<18){
+			imcNominal = "DELGADO";
+		} else if (imcNumerico>=18 &&  imcNumerico<25){
+			imcNominal = "IDEAL";
+		}else if (imcNumerico>=25 &&  imcNumerico<31){
+			imcNominal = "SOBREPRESO";
+		}else if (imcNumerico>31){
+			imcNominal = "OBESO";
+		}
+		
+		
+		System.out.println("SU IMC ES "+ imcNumerico + " " + imcNominal);
+		
+	}
+}
---
+++ appinitial/src/juegoNumerosAleatorio/JuegoNumerosAleatorio.java
@@ -0,0 +1,87 @@
+package juegoNumerosAleatorio;
+import java.util.Scanner;
+
+public class JuegoNumerosAleatorio {
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			int numeroSecreto = (int) (Math.random() * 100) + 1;
+			int intentos = 6;
+			boolean adivinado = false;
+//			boolean o string elec = false o "";			
+//			function menu() {
+//				  console.log(`start`);
+//				  console.log(`exit`);
+//				  let menu = prompt(``).toLowerCase();
+//				  return;
+//				}
+
+			System.out.println("¡Bienvenido al juego de adivinar el número entre 1 y 100!");
+			System.out.println("Tienes 5 intentos para adivinar el número secreto.");
+
+			// Muestra un mensaje indicando que se busca un número menor.
+//		    private static void mensajeNumeroMenor() {
+//		        System.out.println("Busco un número menor.");
+//		    }
+//
+//		    // Muestra un mensaje indicando que se busca un número mayor.
+//		    private static void mensajeNumeroMayor() {
+//		        System.out.println("Busco un número mayor.");
+//		    }
+//
+//		    // Muestra un mensaje de felicitaciones cuando se adivina el número.
+//		    private static void mensajeAdivinado() {
+//		        System.out.println("¡Enhorabuena! Has adivinado el número secreto.");
+//		    }
+//
+//		    // Muestra un mensaje de Game Over con el número secreto.
+//		    private static void mensajeGameOver(int numeroSecreto) {
+//		        System.out.println("Game Over. El número secreto era " + numeroSecreto + ".");
+//		    }
+			do {
+				// menu()
+//				  switch (menu()) {
+//                  case 1:
+//                      (System.out.print("Ingresa un número: ");
+//                      int numeroUsuario = scanner.nextInt();
+//
+//                      if (numeroUsuario < numeroSecreto) {
+//                          mensajeNumeroMenor();
+//                      } else if (numeroUsuario > numeroSecreto) {
+//                          mensajeNumeroMayor();
+//                      } else {
+//                          mensajeAdivinado();
+//                          adivinado = true;
+//                      }
+//                      break;)			       
+//
+//                  case 2:
+//                      mensajeSalir();
+//                      break;
+//
+//                  default:
+//                      System.out.println("Opción no válida. Por favor, selecciona start o exit.");
+//                      break;
+//              }
+				intentos--;
+				System.out.print("Intento " + intentos + ": Ingresa un número: ");
+				int numeroUsuario = scanner.nextInt();
+
+				if (numeroUsuario < numeroSecreto) {
+					System.out.println("Busco un número mayor.");
+					
+				} else if (numeroUsuario > numeroSecreto) {
+					System.out.println("Busco un número menor.");
+					
+				} else {
+					System.out.println("¡Enhorabuena! Has adivinado el número secreto.");
+					adivinado = true;
+				}
+			} while (intentos > 0 && !adivinado);
+
+			if (!adivinado) {
+				System.out.println("Game Over. El número secreto era " + numeroSecreto + ".");
+			}
+		}
+	}
+
+}
